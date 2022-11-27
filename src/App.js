962843a24b481e83f0e87d9b45b79771f355606f
Adaś <adampekul@gmail.com>
2022-11-27 16:16:21 +0100

    removed react-router and auth components from App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container } from "./Style/StyledComponents";
-import { AuthPanel } from "./Components/Authentication/AuthPanel";
 import { AppDisplay } from "./Components/Display/AppDisplay";
 
-// ReactRouter
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
-
 function App() {
-  const [user, setUser] = useState();
-  // Test Commit
   return (
     <Container>
-      <Router>
-        <Routes>
-          <Route
-            // path="/"
-            // element={
-            //   <PrivateRoute user={user}>
-            //     <AppDisplay />
-            //   </PrivateRoute>
-            // }
-            path="/"
-            element={<AppDisplay />}
-          />
-          {/* <Route
-            path="/auth"
-            element={<AuthPanel setUser={setUser} user={user} />}
-          /> */}
-          <Route path="*" element={<div>PAGE NOT FOUND</div>} />
-        </Routes>
-      </Router>
+      <AppDisplay />
     </Container>
   );
 }
